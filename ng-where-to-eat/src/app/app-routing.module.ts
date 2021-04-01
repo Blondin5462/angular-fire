c5743363cd27cc5023c3964ae9a5451287839dd5
maxime <maxime.leszczynski@gmail.com>
2021-04-01 12:24:53 +0200

    Création d'un champ texte et d'un bouton

--- ng-where-to-eat/src/app/app-routing.module.ts
+++ ng-where-to-eat/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SuggestionFormComponent } from './suggestion-form/suggestion-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: SuggestionFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ng-where-to-eat/src/app/app.module.ts
+++ ng-where-to-eat/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Material
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+import { SuggestionFormComponent } from './suggestion-form/suggestion-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuggestionFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
